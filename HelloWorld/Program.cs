3ac93ab00424ffd0429cea47c752b1bd50ee830b
Viplav Patil <viplavdhande91@gmail.com>
2023-04-04 22:05:57 +0530

    code updated

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
+
 namespace Cal
 {
-
-    class Maths
+    public delegate void delCalc(int a, int b);
+    class Arithmetic
     {
+        public static void Main(string[] args)
+        {
+
+           delCalc delg = delegate (int a, int b)
+            {
+                Console.WriteLine("sum is {0}", a + b);
+
+            };
 
 
+            delg(1, 2);
 
+        }
 
-
-
-
-    };
-
-
+    }
 
 }
