6c31f49a422312c35b7671f5496ad17f5e2d9c0a
Viplav Patil <viplavdhande91@gmail.com>
2023-05-20 10:14:16 +0530

    code changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Reflection;
+
 namespace Cal
 {
 
     class Maths
     {
 
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+        delegate void Printer(); 
+        static void Main() {
+            List<Printer> printers = new List<Printer>(); 
+            
+             
+            
+            for (int i = 0;  i < 10; i++) 
+            { 
+            printers.Add(delegate { Console.WriteLine(i); }); 
+            
+            } 
+            
+            foreach (var printer in printers) 
+            { printer(); 
+            
+            } 
         }
 
 
