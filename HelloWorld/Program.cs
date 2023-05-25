3906abe50d4ae5c9b28fd9002c1af435b4c635fc
Viplav Patil <viplav.patil@nagarro.com>
2023-05-25 12:49:25 +0530

    changes in main

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,28 +3,11 @@
 namespace Cal
 {
 
+
     class Maths
     {
 
-        static void Main() {
-
-           Dictionary<string,int> mydict = new Dictionary<string, int>();
-
-
-            mydict.Add("a", 1);
-                
-            mydict.Add("b", 2);
-            mydict.Add("c", 3);
-                
-            
-            Console.WriteLine(mydict.ToString());
-
-            
-            foreach (var item in mydict) { 
-
-                Console.WriteLine("Key is {0} Value is {1}",item.Key,item.Value  );
-            
-            }
+        static void Main(string[] args) {
 
 
         }
