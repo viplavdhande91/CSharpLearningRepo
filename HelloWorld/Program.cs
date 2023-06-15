bbca833a8ab5b64dd70477b2f42dfad567abebd1
Viplav Patil <viplav.patil@nagarro.com>
2023-06-15 18:50:48 +0530

    changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,23 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-
-namespace Cal
+namespace AsyncAwaitDemo
 {
-
-
-    class Maths
+    internal class Program
     {
-
-        static void Main(string[] args) {
-           
+        static void Main()
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
             Console.ReadLine();
+        }
+
+        public static async void Task1()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 1 Starting..");
+                Thread.Sleep(4000);
+                Console.WriteLine("Task 1 Completed..");
+            });
+            Console.WriteLine("Hello Adil....");
+        }
+
+        public static async void Task2()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 2 Starting..");
+                Thread.Sleep(2000);
+                Console.WriteLine("Task 2 Completed..");
+            });
 
         }
 
+        public static async void Task3()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 3 Starting..");
+                Thread.Sleep(5000);
+                Console.WriteLine("Task 3 Completed..");
+            });
 
 
-    };
+        }
 
+        public static async void Task4()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 4 Starting..");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 4 Completed..");
+            });
 
-
+        }
+    }
 }
