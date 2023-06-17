c9049adb1da4aa216f41fbfe2ab1bf760ccbf3c4
Viplav Patil <viplavdhande91@gmail.com>
2023-06-17 17:56:28 +0530

    CHANGES

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,21 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Cal
+namespace EXPLICIT_IMPLEMENTATION_OF_INTERFACE
 {
+    interface i1
+    {
+        void show();
+    }
+    interface i2
+    {
+        void show();
+    }
 
-
-    class Maths
+    class MyClass : i1, i2
     {
 
-        static void Main(string[] args) {
-
-
+        public void show()
+        {
+            Console.WriteLine("this is a method of interface i1 !!");
         }
 
+        void i2.show()   //EXPLICIT DECLARATION
+        {
+            Console.WriteLine("this is a method of interface i2 !!");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
 
-    };
+
+            MyClass obj = new MyClass();
+            obj.show();
+            //((i2)obj).show();  //EXPLICIT CALLING
+
+            i2 obj1 = new MyClass();
+            obj1.show();
 
 
+            //((i1)obj).show();
+            //((i2)obj).show();
 
+            //i1 obj1 = new MyClass();
+            //obj1.show();
+
+            //i2 obj2 = new MyClass();
+            //obj2.show();
+
+            Console.ReadLine();
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# C-Learning" 
+- There are 2 types of implementation of interfaces.
+  1. Implicit Interface Implementation
+  2. Explicit Interface Implementation.
+- A class has to explicitly implement multiple interfaces if these interfaces have methods with identical names.
+- If an interface has a method name identical to the name of a method declared in the inheriting class, this interface has to be explicitly implemented.
