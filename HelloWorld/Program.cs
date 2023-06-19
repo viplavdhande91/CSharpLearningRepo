c03e58fce7382d9b248c357aa3b14993d8852f57
Viplav Patil <viplav.patil@nagarro.com>
2023-06-19 22:26:39 +0530

    partial changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,22 +1,97 @@
 ﻿using System;
 
-
-namespace Cal
+//DERIVED DECORATOR CLASS
+namespace DecoratorPattern
 {
-
-
-    class Maths
+    //BASE INTERFACE
+    interface IPizza
     {
+        string GetPizzaType();
+    }
+    // CONCRETE CLASS
+    class Pizza : IPizza
+    {
+        public string GetPizzaType()
+        {
+            return "This is normal Pizza";
+        }
+    }
 
-        static void Main(string[] args) {
-           
-            Console.ReadLine();
+    //BASE DECORATOR CLASS
 
+    class BasePizzaDecorator : IPizza
+    {   
+        private IPizza _pizza;
+
+        public BasePizzaDecorator(IPizza pizza)
+        {
+            _pizza = pizza;
         }
 
+        public virtual string GetPizzaType()
+        {
+            return "this is nothing";
+        }
+    }
 
 
-    };
+    class cheeseDecorator : BasePizzaDecorator
+    {
+        public cheeseDecorator(IPizza pizza) : base(pizza)
+        {
+        }
+
+        public override string GetPizzaType()
+        {
+            string type = base.GetPizzaType();
+            type += "\nwith extra cheese";
+            return type;
+        }
+    }
+
+    class TomatoDecorator : BasePizzaDecorator
+    {
+        public TomatoDecorator(IPizza pizza) : base(pizza)
+        {
+        }
+
+        public override string GetPizzaType()
+        {
+            string type = base.GetPizzaType();
+            type += "\nwith extra Tomato";
+            return type;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args) { 
+         IPizza pizza = new Pizza();
+         IPizza cheesePizaa = new cheeseDecorator(pizza);
+
+         IPizza tomatoAddedPizza = new TomatoDecorator(cheesePizaa);
+
+         Console.WriteLine(tomatoAddedPizza.GetPizzaType());
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ HelloWorld/cheeseDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class cheeseDecorator : BasePizzaDecorator
+    {
+        public cheeseDecorator(IPizza pizza) : base(pizza)
+        {
+        }
+
+        public override string GetPizzaType()
+        {
+            string type = base.GetPizzaType();
+            type += "\nwith extra cheese";
+            return type;
+        }
+    }
+}
