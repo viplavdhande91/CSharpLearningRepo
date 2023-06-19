c5e6360030ff21f09cf1c299696e92592553c8c6
Viplav Patil <viplavdhande91@gmail.com>
2023-06-19 20:06:30 +0530

    CHANGES

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,21 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
-namespace Cal
+namespace HelloWorld
 {
-
-
-    class Maths
+    public class Program
     {
+        /// Singleton Design Pattern
 
-        static void Main(string[] args) {
+        public static void Main()
+        {
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+            var b4 = LoadBalancer.GetLoadBalancer();
 
+            // Confirm these are the same instance
 
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                WriteLine("Same instance\n");
+            }
+
+            // Next, load balance 15 requests for a server
+
+            var balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                WriteLine("Dispatch request to: " + serverName);
+            }
+
+            // Wait for user
+
+            ReadKey();
         }
+    }
 
-
-
-    };
-
-
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
 
 }
