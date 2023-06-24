c6909ece6c93efac90d46b5fa1e1e79417170ebb
Viplav Patil <viplav.patil@nagarro.com>
2023-06-24 12:58:12 +0530

    changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,10 +8,16 @@ namespace Cal
     class Maths
     {
 
-        static void Main(string[] args) {
-           
-            Console.ReadLine();
-
+        public static void GetNextName(ref int id)
+        {
+        id += 1;
+        }
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Previous value of integer i:" + i.ToString());
+             GetNextName(ref i);
+            Console.WriteLine("Current value of integer i:" + i.ToString());
         }
 
 
@@ -21,3 +27,37 @@ namespace Cal
 
 
 }
+
+//using System;
+
+
+//namespace Cal
+//{
+
+
+//    class Maths
+//    {
+
+//        public static void GetNextName(out int id, out int jd)
+//        {
+
+//            id = 1;
+//            jd = 2;
+
+//        }
+//        static void Main(string[] args)
+//        {
+//            int id;
+//            int jd;
+//            GetNextName(out id, out jd);
+
+//            Console.WriteLine($"{id}   {jd}");
+//        }
+
+
+
+//    };
+
+
+
+//}
