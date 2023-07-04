6dfb93988562ed2a0868afcce827c6d16b12d5b2
Viplav Patil <viplav.patil@nagarro.com>
2023-07-04 11:43:48 +0530

    changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,9 +8,40 @@ namespace Cal
     class Maths
     {
 
+
+        public static int fibonacci(int number) {
+
+            if (number == 0 || number == 1)
+                return number;
+
+            else { 
+            
+            
+            return fibonacci(number - 1) + fibonacci(number-2);
+            }
+
+        
+        
+        
+        
+        }
+        
+
+
+
+
+
         static void Main(string[] args) {
-           
-            Console.ReadLine();
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", fibonacci(i));
+            }
+            Console.ReadKey();
+
+
+
 
         }
 
