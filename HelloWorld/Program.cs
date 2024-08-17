98c68f513b1b0bbcbbf31185197719f64a045611
Viplav Patil <viplavdhande91@gmail.com>
2024-08-17 22:15:45 +0530

    changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,63 +1,46 @@
 ﻿using System;
 
-
-namespace Cal
-{
-
-
-    class Maths
-    {
-
-        public static void GetNextName(ref int id)
-        {
-        id += 1;
-        }
-        static void Main(string[] args)
-        {
-            int i = 1;
-            Console.WriteLine("Previous value of integer i:" + i.ToString());
-             GetNextName(ref i);
-            Console.WriteLine("Current value of integer i:" + i.ToString());
-        }
-
-
-
-    };
-
-
-
-}
-
-//using System;
-
-
 //namespace Cal
 //{
-
 
 //    class Maths
 //    {
 
-//        public static void GetNextName(out int id, out int jd)
+//        public static void GetNextName(ref int id)
 //        {
-
-//            id = 1;
-//            jd = 2;
-
+//        id += 1;
 //        }
 //        static void Main(string[] args)
 //        {
-//            int id;
-//            int jd;
-//            GetNextName(out id, out jd);
-
-//            Console.WriteLine($"{id}   {jd}");
+//            int i = 1;
+//            Console.WriteLine("Previous value of integer i:" + i.ToString());
+//             GetNextName(ref i);
+//            Console.WriteLine("Current value of integer i:" + i.ToString());
 //        }
-
-
 
 //    };
 
-
-
 //}
+
+
+namespace Cal
+{
+    class Maths
+    {
+        public static void GetNextName(out int id, out int jd)
+        {
+
+            id = 1;
+            jd = 2;
+
+        }
+        static void Main(string[] args)
+        {
+            int id;
+            int jd;
+            GetNextName(out id, out jd);
+            Console.WriteLine($"{id}   {jd}");
+        }
+
+    };
+}
