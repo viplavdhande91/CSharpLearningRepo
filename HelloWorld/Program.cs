3a5a006f1b18cb3a7b304cabfb12fcd48f654b61
Viplav Patil <viplavdhande91@gmail.com>
2024-12-13 17:58:15 +0530

    .NET version change

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,22 +1,16 @@
-﻿using System;
-
-namespace Cal
+﻿namespace Cal
 {
     public delegate void delCalc(int a, int b);
     class Arithmetic
     {
         public static void Main(string[] args)
         {
-
-           delCalc delg = delegate (int a, int b)
-            {
-                Console.WriteLine("sum is {0}", a + b);
-
-            };
-
+            delCalc delg = delegate (int a, int b)
+             {
+                 Console.WriteLine("sum is {0}", a + b);
+             };
 
             delg(1, 2);
-
         }
 
     }
