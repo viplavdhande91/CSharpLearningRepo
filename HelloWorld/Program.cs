e08bcb43808f9e9867466b885682b3cd728e33db
Viplav Patil <viplavdhande91@gmail.com>
2024-12-13 17:53:46 +0530

    Changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Cal
+﻿namespace Cal
 {
     public delegate void DelCalc(int a, int b);
 
@@ -9,44 +8,40 @@ namespace Cal
         public static void Addition(int a, int b)
         {
 
-            Console.WriteLine("Result of Addition is {0}",a+b);
+            Console.WriteLine("Result of Addition is {0}", a + b);
 
 
         }
         public static void Subtraction(int a, int b)
         {
 
-            Console.WriteLine("Result of Subtraction is {0}",a-b);
+            Console.WriteLine("Result of Subtraction is {0}", a - b);
 
 
         }
         public static void Multiplication(int a, int b)
         {
 
-            Console.WriteLine("Result of Multiplication is {0}",a*b);
+            Console.WriteLine("Result of Multiplication is {0}", a * b);
         }
 
 
 
-         
-        public static void Main(string[] args){
+
+        public static void Main(string[] args)
+        {
 
 
             DelCalc delc = new DelCalc(Addition);
 
             delc += Subtraction;
 
-            delc+= Multiplication;
+            delc += Multiplication;
 
             delc.Invoke(2, 5);
 
             // delc(2,3);
-           
 
         }
-
-
-    } 
-
-
+    }
 }
