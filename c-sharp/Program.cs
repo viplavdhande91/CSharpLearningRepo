93c84518656dcab51d62eb7ceb4dc33bcabf6c05
Viplav Patil <viplavdhande91@gmail.com>
2025-01-03 10:24:33 +0530

    Program.cs edited by removing main

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,41 +1,9 @@
-﻿using System;
-
-namespace Cal
-{   class Maths
+﻿namespace Cal;
+class Maths
+{
+    static void Main(string[] args)
     {
+        Console.WriteLine("This is Main Method");
+    }
 
-        public static int fibonacci(int number) {
-
-            if (number == 0 || number == 1)
-                return number;
-
-            else { 
-            
-            
-            return fibonacci(number - 1) + fibonacci(number-2);
-            }
-
-          
-        }
-        
-        static void Main(string[] args) {
-            Console.Write("Enter the length of the Fibonacci Series: ");
-            int length = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < length; i++)
-            {
-                Console.Write("{0} ", fibonacci(i));
-            }
-            Console.ReadKey();
-
-
-
-
-        }
-
-
-
-    };
-
-
-
-}
+};
