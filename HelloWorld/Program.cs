f24e89f22d59957185de5ed0a3e8297fa3b9fc34
Viplav Patil <viplavdhande91@gmail.com>
2025-01-24 19:06:06 +0530

    Edit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,37 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿namespace AsyncAwaitDemo;
 
-namespace AsyncAwaitDemo
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main()
+        var p = new Progress<int>(progress =>
         {
-            IProgress<int> p = new Progress<int>(progress =>
-            {
-                Console.WriteLine("Running Step: {0}", progress);
-            });
-            LongJob(p);
-            Console.ReadLine();
+            Console.WriteLine("Running Step: {0}", progress);
+        });
 
-        }
-
-        public static void LongJob(IProgress<int> progress)
-        {
-            var max = 10;
-            for (int i = 0; i < max; i++)
-            {
-                Task.Delay(1000).Wait();
-                
-                progress.Report(i);
-                
-            }
-        }
-
-
+        LongJob(p);
+        Console.ReadLine();
     }
+
+    public static void LongJob(IProgress<int> p1)
+    {
+        var max = 10;
+        for (int i = 0; i < max; i++)
+        {
+            Task.Delay(1000).Wait();
+            p1.Report(i);
+
+        }
+    }
+
 }
--- README.md
+++ README.md
@@ -5,18 +5,3 @@
 ### Benefit
 -  Long-running operations are typically set up as asynchronous methods to prevent the application from blocking these long processes. In this scenario, it is helpful to have a mechanism to report the progress back to the user, to give them an expectation of the running time. The IProgress interface can be used for that purpose.
 
-### Note:
-
-- Using IProgress
-It's important to note that the **System.Progress<T>** class does not have the **Report()** method available on it. This method was implemented explicitly from the IProgress<T> interface, and therefore must be called on a Progress<T> when it's cast to an IProgress<T>.
-
-```csharp
-var p1 = new Progress<int>();
-p1.Report(1); //compiler error, Progress does not contain method 'Report'
-
-IProgress<int> p2 = new Progress<int>();
-p2.Report(2); //works
-
-var p3 = new Progress<int>();
-((IProgress<int>)p3).Report(3); //works
-```
