10bc617acc7b65ffc74a282bea12522fbc0504bc
Viplav Patil <viplavdhande91@gmail.com>
2025-01-24 18:53:00 +0530

    Stocks file delete

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,68 +1,59 @@
-﻿using HelloWorld;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿
+namespace AsyncAwaitDemo;
 
-namespace AsyncAwaitDemo
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main()
+        Task task = Task1();
+        task = Task2();
+        task = Task3();
+        task = Task4();
+        Console.ReadLine();
+    }
+
+    public static async Task Task1()
+    {
+        await Task.Run(() =>
         {
-            //Stocks.stocksMethod();
-            Task1();
-            Task2();
-            Task3();
-            Task4();
-            Console.ReadLine();
-        }
+            Console.WriteLine("Task 1 Starting..");
+            Thread.Sleep(4000);
+            Console.WriteLine("Task 1 Completed..");
+        });
+        Console.WriteLine("Hello Adil....");
+    }
 
-        public static async void Task1()
+    public static async Task Task2()
+    {
+        await Task.Run(() =>
         {
-            await Task.Run(() =>
-            {
-                Console.WriteLine("Task 1 Starting..");
-                Thread.Sleep(4000);
-                Console.WriteLine("Task 1 Completed..");
-            });
-            Console.WriteLine("Hello Adil....");
-        }
+            Console.WriteLine("Task 2 Starting..");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 2 Completed..");
+        });
 
-        public static async void Task2()
+    }
+
+    public static async Task Task3()
+    {
+        await Task.Run(() =>
         {
-            await Task.Run(() =>
-            {
-                Console.WriteLine("Task 2 Starting..");
-                Thread.Sleep(2000);
-                Console.WriteLine("Task 2 Completed..");
-            });
+            Console.WriteLine("Task 3 Starting..");
+            Thread.Sleep(5000);
+            Console.WriteLine("Task 3 Completed..");
+        });
 
-        }
 
-        public static async void Task3()
+    }
+
+    public static async Task Task4()
+    {
+        await Task.Run(() =>
         {
-            await Task.Run(() =>
-            {
-                Console.WriteLine("Task 3 Starting..");
-                Thread.Sleep(5000);
-                Console.WriteLine("Task 3 Completed..");
-            });
+            Console.WriteLine("Task 4 Starting..");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 4 Completed..");
+        });
 
-
-        }
-
-        public static async void Task4()
-        {
-            await Task.Run(() =>
-            {
-                Console.WriteLine("Task 4 Starting..");
-                Thread.Sleep(1000);
-                Console.WriteLine("Task 4 Completed..");
-            });
-
-        }
     }
 }
